2d799b1659ec2fcfae7e5a005505d1fe98c546fc
Pham Ngoc Hai <hai.pn@ARC-HaiPN>
2021-03-29 10:26:45 +0700

    Initial Vue setup

---
+++ client/src/store/index.js
@@ -0,0 +1,19 @@
+import createPersitedState from 'vuex-persistedstate';
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  plugins: [
+    createPersitedState(),
+  ],
+  modules: {
+  },
+});
